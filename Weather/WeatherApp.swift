c639b8c9b1eaa3a20374a7d187c7700a7e2188ce
Ahmet Ozkan <ahmet.ozkan@sebit.com.tr>
2023-02-18 20:04:16 +0300

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Ahmet Özkan on 18.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
