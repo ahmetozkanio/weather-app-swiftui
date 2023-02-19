ff4f8480d4ee9ad42a389f8b5228b3745f0ed8cc
Ahmet Ozkan <ahmet.ozkan@sebit.com.tr>
2023-02-19 22:02:57 +0300

    ForecastView Animated Scroll added

--- Weather/Views/Main/HomeView.swift
+++ Weather/Views/Main/HomeView.swift
@@ -15,64 +15,89 @@ enum BottomSheetPosition: CGFloat, CaseIterable{
 
 struct HomeView: View {
     @State var bottomSheetPosition: BottomSheetPosition = .middle
+    @State var bottomSheetTranslation: CGFloat = BottomSheetPosition.middle.rawValue
+    @State var hasDragged: Bool = false
     
+    var bottomSheetTranslationProated: CGFloat{
+        (bottomSheetTranslation - BottomSheetPosition.middle.rawValue) / (BottomSheetPosition.top.rawValue - BottomSheetPosition.middle.rawValue)
+    }
     var body: some View {
         NavigationView {
-            ZStack{
-                // MARK: Backgound Color
-                Color.background
-                    .ignoresSafeArea()
-                
-                // MARK: Background Image
-                Image("Background")
-                    .resizable()
-                    .ignoresSafeArea()
-                
-                // MARK: House Image
-                Image("House")
-                    .frame(maxHeight: .infinity,alignment: .top)
-                    .padding(.top,257)
-                
-                VStack(spacing: -10) {
-                    Text("Montreal")
-                        .font(.largeTitle)
+            GeometryReader { geometry in
+                let screenHeight = geometry.size.height + geometry.safeAreaInsets.top + geometry.safeAreaInsets.bottom
+                let imageOffset = screenHeight + 36
+                ZStack{
+                    // MARK: Backgound Color
+                    Color.background
+                        .ignoresSafeArea()
                     
-                    VStack{
-                        Text(attributedString)
+                    // MARK: Background Image
+                    Image("Background")
+                        .resizable()
+                        .ignoresSafeArea()
+                        .offset(y: -bottomSheetTranslationProated * imageOffset)
+                    
+                    // MARK: House Image
+                    Image("House")
+                        .frame(maxHeight: .infinity,alignment: .top)
+                        .padding(.top,257)
+                        .offset(y: -bottomSheetTranslationProated * imageOffset)
+                    
+                    VStack(spacing: -10 * (1 - bottomSheetTranslationProated)) {
+                        Text("Montreal")
+                            .font(.largeTitle)
                         
-                        Text("H:24'    L:18'")
-                            .font(.title3.weight(.semibold))
+                        VStack{
+                            Text(attributedString)
+                            
+                            Text("H:24'    L:18'")
+                                .font(.title3.weight(.semibold))
+                                .opacity(1 - bottomSheetTranslationProated)
+                        }
+                        Spacer()
                     }
-                    Spacer()
+                    .padding(.top,51)
+                    .offset(y: -bottomSheetTranslationProated * 46)
+                    
+                    // MARK: Bottom Sheet
+                    BottomSheetView(position: $bottomSheetPosition){
+                        Text(bottomSheetTranslationProated.formatted())
+                    } content: {
+                        ForecastView(bottomSheetTranslationProrated: bottomSheetTranslationProated)
+                    }
+                    .onBottomSheetDrag{ translation in
+                        bottomSheetTranslation = translation / screenHeight
+                        
+                        withAnimation(.easeOut){
+                            if bottomSheetPosition == BottomSheetPosition.top{
+                                hasDragged = true
+                                
+                            }else{
+                                hasDragged = false
+                            }
+                        }
+                    }
+                    // MARK: Tab Bar
+                    TabBar(action: {
+                        bottomSheetPosition = .top
+                    })
+                    .offset(y: bottomSheetTranslationProated * 156)
                 }
-                .padding(.top,51)
-                
-                // MARK: Bottom Sheer
-                BottomSheetView(position: $bottomSheetPosition){
-//                    Text(bottomSheetPosition.rawValue.formatted())
-                } content: {
-                    ForecastView()
-                }
-                
-                // MARK: Tab Bar
-                TabBar(action: {
-                    bottomSheetPosition = .top
-                })
+                .navigationBarHidden(true)
             }
-            .navigationBarHidden(true)
         }
     }
     
     private var attributedString: AttributedString{
-        var string = AttributedString("19'" + "\n " + "Mostly Clear")
+        var string = AttributedString("19'" + (hasDragged ? " | " :  "\n ") + "Mostly Clear")
         
         if let temp = string.range(of: "19'") {
-            string[temp].font = .system(size: 96,weight: .thin)
-            string[temp].foregroundColor = .primary
+            string[temp].font = .system(size: (96 - (bottomSheetTranslationProated * (96 - 20))) ,weight: hasDragged ? .semibold : .thin)
+            string[temp].foregroundColor = hasDragged ? .secondary : .primary
         }
         if let pipe = string.range(of: " | ") {
             string[pipe].font = .title3.weight(.semibold)
-            string[pipe].foregroundColor = .secondary
+            string[pipe].foregroundColor = .secondary.opacity(bottomSheetTranslationProated)
         }
         if let weather = string.range(of: "Mostly Clear") {
             string[weather].font = .title3.weight(.semibold)
